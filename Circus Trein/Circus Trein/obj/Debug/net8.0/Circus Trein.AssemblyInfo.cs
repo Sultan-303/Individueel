7ad847223461b9c4249cc2198ea03f9a7e0e8eb4
Sultan <531213@student.fontys.nl>
2024-03-18 09:22:11 +0100

    Second Commit

--- Circus Trein/Circus Trein/obj/Debug/net8.0/Circus Trein.AssemblyInfo.cs
+++ Circus Trein/Circus Trein/obj/Debug/net8.0/Circus Trein.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Circus Trein")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6ea421206dd168480dc9cb2924510b963e91c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Circus Trein")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Circus Trein")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
